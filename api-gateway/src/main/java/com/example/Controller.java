03c83bdb42e8e3c6262a4c1f7f894ddcea0f5d2d
nn1ck0 <gnidenkonn@gmail.com>
2025-06-28 12:09:56 +0300

    Изменен контроллер: если сервис недоступен, то эту ситуацию обрабатывает один метод

--- api-gateway/src/main/java/com/example/Controller.java
+++ api-gateway/src/main/java/com/example/Controller.java
@@ -2,8 +2,6 @@ package com.example;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fallback/users")
 public class Controller {
 
-    @GetMapping()
-    public ResponseEntity<String> getUsers() {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Unavailable");
-    }
-
-    @PostMapping
-    public ResponseEntity<String> postUsers() {
+    @RequestMapping
+    public ResponseEntity<String> getFallback() {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Unavailable");
     }
 }
