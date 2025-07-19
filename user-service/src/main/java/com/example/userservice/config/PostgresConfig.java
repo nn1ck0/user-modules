f1521481050aa2c88b24abb2e8ae9f07fac398c0
nn1ck0 <gnidenkonn@gmail.com>
2025-07-19 12:52:22 +0300

    попытка настроить production.env

--- user-service/src/main/java/com/example/userservice/config/PostgresConfig.java
+++ user-service/src/main/java/com/example/userservice/config/PostgresConfig.java
@@ -6,16 +6,16 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class PostgresConfig {
-    @Bean
-    public DataSource dataSource() {
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:13000/userservicedb");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
-        return dataSource;
-    }
-}
+//@Configuration
+//public class PostgresConfig {
+//    @Bean
+//    public DataSource dataSource() {
+//
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:13000/userservicedb");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//        return dataSource;
+//    }
+//}
