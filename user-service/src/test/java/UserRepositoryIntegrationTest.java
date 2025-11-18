1ea1be2de0a9772ee132b9272c18f8bc7ad9a1bb
nn1ck0 <gnidenkonn@gmail.com>
2025-11-18 21:44:33 +0300

    gradle

--- user-service/src/test/java/UserRepositoryIntegrationTest.java
+++ user-service/src/test/java/UserRepositoryIntegrationTest.java
@@ -1,63 +1,63 @@
-import com.example.userservice.UserServiceApplication;
-import com.example.userservice.entity.UserEntity;
-import com.example.userservice.repo.UserRepo;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
-
-
-@Testcontainers
-@TestPropertySource(locations = "classpath:application-test.yaml")
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@SpringBootTest(classes = UserServiceApplication.class)
-@ActiveProfiles("test")
-class UserRepositoryIntegrationTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
-        .withDatabaseName("userservicedb")
-        .withUsername("postgres")
-        .withPassword("postgres");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        if (!postgres.isRunning()) {
-            postgres.start();
-        }
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
-        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
-    }
-
-
-    @Autowired
-    private UserRepo userRepo;
-
-    @Test
-    public void shouldFindByEmail() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setFirstName("John");
-        userEntity.setLastName("Doe");
-        userEntity.setEmail("test@test.com");
-        userRepo.save(userEntity);
-
-        Optional<UserEntity> user = userRepo.findByEmail("test@test.com");
-        assertThat(user).isPresent();
-    }
-}
+//import com.example.userservice.UserServiceApplication;
+//import com.example.userservice.entity.UserEntity;
+//import com.example.userservice.repo.UserRepo;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.springframework.test.context.TestPropertySource;
+//import org.testcontainers.containers.PostgreSQLContainer;
+//import org.testcontainers.junit.jupiter.Container;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//
+//import java.util.Optional;
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//
+//@Testcontainers
+//@TestPropertySource(locations = "classpath:application-test.yaml")
+////@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@SpringBootTest(classes = UserServiceApplication.class)
+//@ActiveProfiles("test")
+//class UserRepositoryIntegrationTest {
+//
+//    @Container
+//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+//        .withDatabaseName("userservicedb")
+//        .withUsername("postgres")
+//        .withPassword("postgres");
+//
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        if (!postgres.isRunning()) {
+//            postgres.start();
+//        }
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
+//        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+//        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
+//    }
+//
+//
+//    @Autowired
+//    private UserRepo userRepo;
+//
+//    @Test
+//    public void shouldFindByEmail() {
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setFirstName("John");
+//        userEntity.setLastName("Doe");
+//        userEntity.setEmail("test@test.com");
+//        userRepo.save(userEntity);
+//
+//        Optional<UserEntity> user = userRepo.findByEmail("test@test.com");
+//        assertThat(user).isPresent();
+//    }
+//}
